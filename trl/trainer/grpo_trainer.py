c1fb839a87570617588d5a1ef35b6458165dcd31
Isak Persson <isak@ebbot.ai>
2025-02-05 11:24:04 +0100

    fix maybe

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -444,7 +444,7 @@ class GRPOTrainer(Trainer):
                         if isinstance(unwrapped_model, PeftModel):
                             unwrapped_model = copy.deepcopy(unwrapped_model)
                             unwrapped_model.merge_and_unload()
-                            state_dict = unwrapped_model.state_dict()
+                        state_dict = unwrapped_model.state_dict()
 
                     if self.accelerator.is_main_process:
                         llm_model = self.llm.llm_engine.model_executor.driver_worker.model_runner.model
